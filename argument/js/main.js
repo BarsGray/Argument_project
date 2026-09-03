4cbe63ca3309e45c68c40feee312f01779d5484e
Daniil <m2140sr@gmail.com>
2026-09-03 17:43:28 +0300

    day_3

---
+++ argument/js/main.js
@@ -0,0 +1,31 @@
+// ======================= footer_menu ==========================
+const footerMenuLinkList = document.querySelectorAll('.footer_mid nav>ul>li.menu-item-has-children a');
+
+function resetSubMenu() {
+  footerMenuLinkList.forEach((link) => {
+    if (!link.nextElementSibling) return;
+    link.classList.remove('active');
+    link.nextElementSibling.classList.remove('active');
+    link.nextElementSibling.removeAttribute('style');
+  });
+}
+
+footerMenuLinkList.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+  console.log('click');
+    if (window.innerWidth > 1000) return;
+    
+    const subMenu = e.currentTarget.nextElementSibling;
+    if (!subMenu) return;
+    const isOpen = subMenu.classList.contains('active');
+    
+    resetSubMenu();
+    
+    if(!isOpen) {
+      subMenu.classList.add('active');
+      e.currentTarget.classList.add('active');
+      subMenu.setAttribute('style', 'max-height:' + subMenu.scrollHeight + 'px;');
+    }
+  });
+});
