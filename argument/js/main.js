b8014eab20fde00d856f2d0c5aa28cc7f465be81
Daniil <m2140sr@gmail.com>
2026-09-14 15:04:55 +0300

    day_9

--- argument/js/main.js
+++ argument/js/main.js
@@ -1,32 +1,48 @@
 // ======================= swiper_bunner ==========================
-  const bunner_swiper = new Swiper('.bunner_swiper', {
-    loop: true,
-    centeredSlides: false,
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: ".bunner_btn_next",
-      prevEl: ".bunner_btn_prev"
-    },
-    pagination: {
-      el: ".bunner_pagination",
-      clickable: true,
-    }
-  });
+const bunner_swiper = new Swiper('.bunner_swiper', {
+  loop: true,
+  centeredSlides: false,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".bunner_btn_next",
+    prevEl: ".bunner_btn_prev"
+  },
+  pagination: {
+    el: ".bunner_pagination",
+    clickable: true,
+  }
+});
+// ======================= tubs =======================
+const tubs_row = document.querySelector('.services_tubs_row');
+
+if (tubs_row) {
+  const activeTab = tubs_row.querySelector('.active');
+
+  tubs_row.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    const direction = e.deltaY > 0 ? 1 : -1;
+    const scrollStep = 200;
+
+    tubs_row.scrollBy({ left: direction * scrollStep, behavior: 'smooth' });
+  }, { passive: false });
+}
 // ======================= marquee ==========================
-  const marquee = document.querySelector('.labels_marquee');
-  const track = document.querySelector('.labels_track');
-  const original = document.querySelector('.labels_list');
-  const label_elem = document.querySelectorAll('.labels_list li');
+const marquee = document.querySelector('.labels_marquee');
+const track = document.querySelector('.labels_track');
+const original = document.querySelector('.labels_list');
+const label_elem = document.querySelectorAll('.labels_list li');
 
+if (marquee && label_elem ) {
   const count = Math.ceil(3000 / original.scrollWidth) + Math.floor(3000 / (label_elem[0].scrollWidth * label_elem.length));
-
+  
   for (let i = 0; i < count; i++) {
     const clone = original.cloneNode(true);
     clone.setAttribute('aria-hidden', 'true');
     track.appendChild(clone);
   }
+}
 // ======================= footer_menu ==========================
 const footerMenuLinkList = document.querySelectorAll('.footer_mid nav>ul>li.menu-item-has-children a');
 
